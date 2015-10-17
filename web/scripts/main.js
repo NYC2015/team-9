f2da91d3e7cbbf8c20f2c613256d25e358bca20e
Basil Vetas <basilvetas@gmail.com>
2015-10-17 00:22:18 -0400

    working

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,11 +1,17 @@
+'use strict';
 Parse.initialize("h840VqB3Hj6oaiccjPXFrlj86jwu1ecCmv0N8pc4", "mupd3jOW4P0UdkQScPhn11Lhhg7uknxURWkbtV9Y");
 
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}).then(function(object) {
+// alert("yay! it worked");
+// });
 
+/* Controllers */
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-alert("yay! it worked");
+var rockEarthApp = angular.module('rockEarthApp', []);
+
+rockEarthApp.controller('MainController', function($scope) {
+	$scope.test = "hello";
 });
-
 
