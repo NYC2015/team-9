13ec390b001e4daacfad61c44e494dacde5eee8b
Basil Vetas <basilvetas@gmail.com>
2015-10-17 08:33:06 -0400

    need to pull from db

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,5 +1,5 @@
 'use strict';
-Parse.initialize("h840VqB3Hj6oaiccjPXFrlj86jwu1ecCmv0N8pc4", "mupd3jOW4P0UdkQScPhn11Lhhg7uknxURWkbtV9Y");
+Parse.initialize("8Zmxhf0z8gYpTzVPOIThIULbEo04hX3NT7bFvRx1", "l0ctJ43ZGTh9GqErJqAweKSF29zNzOBMUK6LfdRJ");
 
 // var TestObject = Parse.Object.extend("TestObject");
 // var testObject = new TestObject();
@@ -13,8 +13,53 @@ var rockEarthApp = angular.module('rockEarthApp', []);
 
 rockEarthApp.controller('MainController', ['$scope', function($scope) {
 	$scope.name = "Close Indian Point Nuclear Plant Now";
-	$scope.number = 8;
+	$scope.number = 2;
 	$scope.count = 1234;
+	var data = [];
+	var myResults;
+
+	var TestObject = Parse.Object.extend("pictureTest");
+	var query = new Parse.Query(TestObject);
+
+	query.equalTo("campaign", "water");
+	query.find({
+	  success: function(results) {	  	
+
+	  	// console.log(results);
+	    // alert("Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	    for (var i = 0; i < $scope.number; i++) {
+	      var object = results[i];
+	      // alert(object.id + ' - ' + object.get('objectId'));
+
+	      var imageFile = object.get('testPictures');
+
+	      var imgId = 'img' + i.toString();
+	      // console.log(imgId);
+	      // document.getElementById(imgId).src 
+	      document.getElementById(imgId).src = imageFile.url();	      
+	      // console.log(data);		      
+	    }
+
+	    // $scope.$apply(function(results)
+	    // {
+	    // 	$scope.objects = results.map(function(obj)
+	    // 	{
+	    // 		return {url: obj.get('testPictures'),
+	    // 		parseObject: obj};
+	    // 	});
+
+	    // });
+
+	    // $scope.objects = data;	    
+	    // console.log($scope.objects);
+
+
+	  },
+	  error: function(error) {
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
 
 	// $scope.objects = {}; // make http request for objects
 }]);
@@ -29,7 +74,7 @@ rockEarthApp.controller('MainController', ['$scope', function($scope) {
 // });
 
 // $(window).load(function() {
-	
+
 // });
 
 
--- web/scripts/test.js
+++ web/scripts/test.js
@@ -16,16 +16,14 @@ var query = new Parse.Query(TestObject);
 query.equalTo("campaign", "water");
 query.find({
   success: function(results) {
-    alert("Successfully retrieved " + results.length + " scores.");
+    // alert("Successfully retrieved " + results.length + " scores.");
     // Do something with the returned Parse.Object values
     for (var i = 0; i < results.length; i++) {
       var object = results[i];
-      alert(object.id + ' - ' + object.get('objectId'));
+      // alert(object.id + ' - ' + object.get('objectId'));
 
       var imageFile = object.get('testPictures');
       document.getElementById("test1").src = imageFile.url();
-
-
     }
   },
   error: function(error) {
